6b0f62dff538043ae83799a4b8c01904832010ac
Ammar Ali <funnyguy118@gmail.com>
2019-01-25 03:08:44 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,9 @@
 def oxford_comma(array)
-  if array.length > 2 
+  if array.length == 2 
+    return array.join(" and ")
+  elsif 2 < array.length
     and_finish = "and #{array.pop}"
     array.push(and_finish)
-    array.join(", ")
-  elsif array.length == 2 
-    array.join(" and ")
-  else
-    array.join
-  end 
+  end
+  array.join(", ")
 end
