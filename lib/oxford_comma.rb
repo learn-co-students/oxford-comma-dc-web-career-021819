7e891bf7d8bfba4aa59822d60ad4965755a09b64
Mickey <micksheridan.24@gmail.com>
2019-01-28 20:36:37 -0500

    submit

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,25 @@
 def oxford_comma(array)
+    if (array.size == 1)
+       return array [0]
+    elsif (array.size == 2)
+        return (array[0] + " and " + array[1])
+    else
+        str = ""
 
+        c = 0
+        while c < array.size
+            if (array[c] == array.last)
+                str += "and "
+            end
+
+            str += array [c]
+
+            if (array[c] != array.last)
+                str += ", "
+            end
+            c+=1
+        end
+        return str
+
+    end
 end
